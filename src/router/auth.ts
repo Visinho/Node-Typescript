c5ba9c3c875e5c55ff5d1d4ed39343700fb8ed78
Visinho <Elvisvisinho@rocketmail.com>
2024-01-08 18:22:22 +0100

    Created the login endpoint with cookies

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,8 +1,9 @@
 import express from "express";
 
-import { register } from "../controllers/auth";
+import { register, login } from "../controllers/auth";
 
 export default (router: express.Router) => {
     router.post('/auth/register', register);
+    router.post('/auth/login', login);
 };
 
