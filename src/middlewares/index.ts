6f3c44c3dc926cf7e80d6f2bde592abbccb06122
Visinho <Elvisvisinho@rocketmail.com>
2024-01-08 22:52:10 +0100

    Created the delete user endpoint with the authorization middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -18,6 +18,23 @@ export const isAuthenticated = async(req: express.Request, res: express.Response
          merge(req, { identity: existingUser })
          return next();
     } catch (error) {
-        
+        return res.sendStatus(400);
+    }
+}
+
+export const IsOwner = async(req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, "identity._id") as string;
+
+        if(!currentUserId) {
+            return res.sendStatus(403);
+        }
+        if(currentUserId.toString() !== id) {
+            return res.sendStatus(403);
+        }
+        next();
+    } catch (error) {
+        return res.sendStatus(400);
     }
 }
